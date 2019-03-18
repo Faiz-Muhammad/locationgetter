25656928f600278f7d86e7b9fa39814795ff5ac7
Faiz-Muhammad <practice.rubyonrails1@gmail.com>
2019-03-18 20:15:20 +0500

    testing

--- db/migrate/20190318145053_change_columns_into_locations.rb
+++ db/migrate/20190318145053_change_columns_into_locations.rb
@@ -1,6 +1,6 @@
 class ChangeColumnsIntoLocations < ActiveRecord::Migration[5.2]
   def change
-    change_column :locations, :latitude, :float, :limit => 25
-    change_column :locations, :longitude, :float, :limit => 25
+    change_column :locations, :latitude, :decimal, :precision => 15, :scale => 10
+    change_column :locations, :longitude, :decimal, :precision => 15, :scale => 10
   end
 end
