31032548a8151e6f706776f8753d42d4f9ad701b
Faiz-Muhammad <practice.rubyonrails1@gmail.com>
2019-03-18 17:14:52 +0500

    completed!

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -7,17 +7,16 @@ class LocationsController < ApplicationController
     # visitor_longitude = 74.27267909999999
     @data = Location.new(longitude: visitor_longitude, latitude: visitor_latitude)
     @data.save
-    redirect_to root_path
   end
 
-  def submit
-    visitor_latitude = request.location.latitude
-    visitor_longitude = request.location.longitude
-  end
+  # def submit
+  #   visitor_latitude = request.location.latitude
+  #   visitor_longitude = request.location.longitude
+  # end
 
-  def location_params
-    params.permit(:longitude, :latitude)
-  end
+  # def location_params
+  #   params.permit(:longitude, :latitude)
+  # end
 
 
 end
