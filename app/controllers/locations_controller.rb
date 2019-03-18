0c0d57570a0ce4d1d102bb44870a4825ac86530d
Faiz-Muhammad <practice.rubyonrails1@gmail.com>
2019-03-18 20:54:34 +0500

    commit

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,10 +1,10 @@
 class LocationsController < ApplicationController
 
   def index
-    visitor_latitude = request.location.latitude
-    visitor_longitude = request.location.longitude
-    # visitor_latitude = 31.462861599999997
-    # visitor_longitude = 74.27267909999999
+    visitor_latitude = request.location.latitude.to_s
+    visitor_longitude = request.location.longitude.to_s
+    # visitor_latitude = 31.462861599999997.to_s
+    # visitor_longitude = 74.27267909999999.to_s
     visitor_location = Geocoder.search([visitor_latitude, visitor_longitude])
     @data = Location.new(longitude: visitor_longitude, latitude: visitor_latitude, location: visitor_location.first.address)
     @data.save
