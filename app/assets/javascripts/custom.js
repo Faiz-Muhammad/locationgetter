58ca6c21a555d7945ed7b06b2f2237ab6a3b7fa5
Faiz-Muhammad <practice.rubyonrails1@gmail.com>
2019-03-18 17:08:27 +0500

    errors fixed!

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,19 +1,19 @@
-$(document).ready(function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    alert("Geolocation is not supported by this browser.");
-  }
-});
+// $(document).ready(function getLocation() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(showPosition);
+//   } else {
+//     alert("Geolocation is not supported by this browser.");
+//   }
+// });
+//
+// function showPosition(position) {
+//   console.log(position.coords.latitude);
+//   console.log(position.coords.longitude);
+//   $.post("/submit",
+//     {
+//       Latitude: position.coords.latitude,
+//       Longitude: position.coords.longitude
+//     });
+// };
 
-function showPosition(position) {
-  console.log(position.coords.latitude);
-  console.log(position.coords.longitude);
-  $.post("/submit",
-    {
-      Latitude: position.coords.latitude,
-      Longitude: position.coords.longitude
-    });
-};
-
-$(document).ready($.post("/submit"));
+// $(document).ready($.post("/submit"));
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,14 +5,14 @@ class LocationsController < ApplicationController
     visitor_longitude = request.location.longitude
     # visitor_latitude = 31.462861599999997
     # visitor_longitude = 74.27267909999999
+    @data = Location.new(longitude: visitor_longitude, latitude: visitor_latitude)
+    @data.save
+    redirect_to root_path
   end
 
   def submit
     visitor_latitude = request.location.latitude
     visitor_longitude = request.location.longitude
-    @data = Location.new(longitude: visitor_longitude, latitude: visitor_latitude)
-    @data.save
-    redirect_to root_path
   end
 
   def location_params
